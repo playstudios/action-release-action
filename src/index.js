a8ef51e568c3fa44b9a28c38c33ad25592f4619b
Alex Moze <alexm@playstudios.asia>
2021-11-23 14:10:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ const release = async () => {
       branches: [branch],
       releaseRules: [{ type: 'build', scope: 'deps', release: 'patch' }],
       preset: 'conventionalcommits',
-      tagFormat: core.getInput('tag-format') || 'v${version}',
+      tagFormat: '${version}',
       plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
