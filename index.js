4799c09c7abe418a1970808695d0c3302736ba9e
hungran <26101787+hungran@users.noreply.github.com>
2023-05-11 23:15:06 +0700

    fix: install node module when action run (#35)
    * fix: install node module when action run
    
    * fix: naming convention
    
    * fix: action should run from index.js
    
    * fix: package-lock.json
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * typo

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+const exec = require('./src/exec')
+const path = require('path')
+
+const run = async () => {
+  // Install Dependencies
+  {
+    const { stdout, stderr } = await exec('npm --loglevel error ci --only=prod', {
+      cwd: path.resolve(__dirname),
+    })
+    core.info(stdout)
+    if (stderr) {
+      return Promise.reject(stderr)
+    }
+  }
+
+  require('./src/index')()
+}
+
+run().catch(core.setFailed)
