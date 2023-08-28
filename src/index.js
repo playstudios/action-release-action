e3fabc8cebbf586b521242d10499784d40265ec6
Henry Vu <26101787+hungran@users.noreply.github.com>
2023-08-28 14:38:55 +0700

    fix: hardcode plugin version in package-lock and package json (#49)

--- src/index.js
+++ src/index.js
@@ -61,12 +61,12 @@ const release = async () => {
       preset: 'conventionalcommits',
       tagFormat: core.getInput('tag-prefix') === 'v' ? 'v${version}' : `${core.getInput('tag-prefix')}\${version}`,
       plugins: [
-        '@semantic-release/commit-analyzer@9.0.2',
-        '@semantic-release/release-notes-generator@10.0.3',
-        '@semantic-release/github@8.1.0',
-        '@semantic-release/changelog@6.0.3',
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+        '@semantic-release/changelog',
         [
-          '@semantic-release/git@10.0.1',
+          '@semantic-release/git',
           {
             assets: ['dist/**', 'CHANGELOG.md'],
           },
