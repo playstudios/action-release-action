f17b49d6c67afdc355e1b1734b689271acd6ccb2
Lucas Nguyen <lucas.nguyen@playstudios.asia>
2025-07-11 10:14:30 +0700

    DO-3174: WIP

--- src/index.js
+++ src/index.js
@@ -30,12 +30,16 @@ const clean = async () => {
   const octokit = github.getOctokit(core.getInput('repo-token'))
   const ref = await octokit.rest.git.getRef({ ...github.context.repo, ref: `heads/${branch}` }).catch((e) => e)
 
+  core.info("Ref data:")
+  core.info(ref.data)
+
   if (ref.data) {
     core.info(`deleting release branch ${ref.data.ref}`)
     await octokit.rest.git.deleteRef({ ...github.context.repo, ref: `heads/${branch}` })
   } else if (ref.status === '404') {
     core.info('release branch of deleted branch not found')
   } else {
+    core.info(`could not delete release branch ${branch}, got error: ${ref}`)
     throw ref
   }
 }
